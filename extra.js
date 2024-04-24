8c8ae814515175bc36ea0cf5e5ab15410a416b4e
ROXAS1321 <maxime.roxas@hotmail.fr>
2024-04-24 23:36:41 +0200

    Change-Color-Button

--- extra.js
+++ extra.js
@@ -3,3 +3,10 @@
     maBaliseDiv=document.getElementByld("jeu")
     console.log(maBaliseDiv)
     // affiche ce qui est contenu dans la balise
+
+    let jeu=document.getElementByld("jeu")
+    jeu.addEventListener("click", function () {
+        let body=document.querySelector("body")
+        // On change la couleur de fond de la page
+        body.style.backgroudColor= "red" 
+    });
